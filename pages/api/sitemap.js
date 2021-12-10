573db37987877b3e8bc702fc7ec54d373d8aac31
hendrik <hendrik.schaefer@ambient.digital>
2021-12-10 15:31:24 +0100

    sitemap

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -3,7 +3,10 @@ const { Readable } = require("stream");
 
 export default async (req, res) => {
   // An array with your links
-  const links = [{ url: "/impressum/", changefreq: "daily", priority: 0.3 }];
+  const links = [
+    { url: "/", changefreq: "daily", priority: 0.3 },
+    { url: "/impressum", changefreq: "daily", priority: 0.3 },
+  ];
 
   // Create a stream to write to
   const stream = new SitemapStream({ hostname: `https://${req.headers.host}` });
