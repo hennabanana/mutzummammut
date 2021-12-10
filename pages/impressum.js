c50e924567d758071b4a17601d438c8e9db3a651
hendrik <hendrik.schaefer@ambient.digital>
2021-12-10 15:22:21 +0100

    robots, sitemap, link, quallenschatten

--- pages/impressum.js
+++ pages/impressum.js
@@ -1,103 +1,161 @@
-import styles from '../styles/Page.module.scss'
-import Head from 'next/head'
-import Link from 'next/link'
-
-
+import styles from "../styles/Page.module.scss";
+import Head from "next/head";
+import Link from "next/link";
 
 function Impressum() {
-    return (
-      <div className={styles.container}>
-        <Head>
-          <title>Mut zum Mammut</title>
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="stylesheet" href="https://use.typekit.net/qvg1efb.css"></link>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
-  
-        <main>
-          <div id={styles.container1} className={styles.impressum_content} >
-            <Link href="/">
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Mut zum Mammut</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://use.typekit.net/qvg1efb.css"
+        ></link>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <main>
+        <div id={styles.container1} className={styles.impressum_content}>
+          <Link href="/">
             <div id={styles.title}>
-            <p>Impressum</p>
-            <h1>Mut zum Mammut</h1>
+              <p>Impressum</p>
+              <h1>Mut zum Mammut</h1>
             </div>
-            </Link>
-            <div id={styles.content}>
-            <p>Angaben gemäß § 5 TMG<br></br>
-Fiona Göbel<br></br>
-Brüsseler Platz 17<br></br>
-50674 Köln<br></br><br></br>
-<b>Vertreten durch:</b><br></br>
-Fiona Göbel<br></br><br></br>
-<b>Kontakt:</b><br></br>
-Telefon: 0049–162 30 85 325<br></br>
-E‑Mail: hallo@mut-zum-mammut.de<br></br><br></br>
-<b>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</b><br></br>
-Fiona Göbel<br></br>
-Brüsseler Platz 17<br></br>
-50674 Köln<br></br><br></br>
-<b>Konzeption und Design:</b><br></br>
-Sara-Lena Göbel<br></br>
-E‑Mail: hola@maniana-design.de<br></br><br></br>
- 
-<b>Haftungsausschluss:</b><br></br><br></br>
-
-<b>Haftung für Inhalte</b><br></br>
-
-Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.<br></br>
-
-<br></br><b>Haftung für Links</b><br></br>
-
-Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.<br></br>
-
-<br></br><b>Urheberrecht</b><br></br>
-
-Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.<br></br>
-
-<br></br><b>Datenschutz</b><br></br>
-
-Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.
-Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E‑Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.<br></br>
-Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.</p>
-            </div>
-          </div>          
-          <div id={styles.container5}>
+          </Link>
+          <div id={styles.content}>
+            <p>
+              Angaben gemäß § 5 TMG<br></br>
+              Fiona Göbel<br></br>
+              Brüsseler Platz 17<br></br>
+              50674 Köln<br></br>
+              <br></br>
+              <b>Vertreten durch:</b>
+              <br></br>
+              Fiona Göbel<br></br>
+              <br></br>
+              <b>Kontakt:</b>
+              <br></br>
+              Telefon: 0049–162 30 85 325<br></br>
+              E-Mail: hallo@mut-zum-mammut.de<br></br>
+              <br></br>
+              <b>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</b>
+              <br></br>
+              Fiona Göbel<br></br>
+              Brüsseler Platz 17<br></br>
+              50674 Köln<br></br>
+              <br></br>
+              <b>Konzeption und Design:</b>
+              <br></br>
+              Sara-Lena Göbel<br></br>
+              E-Mail: hola@maniana-design.de<br></br>
+              <br></br>
+              <b>Haftungsausschluss:</b>
+              <br></br>
+              <br></br>
+              <b>Haftung für Inhalte</b>
+              <br></br>
+              Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt.
+              Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte
+              können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter
+              sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten
+              nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG
+              sind wir als Diensteanbieter jedoch nicht verpflichtet,
+              übermittelte oder gespeicherte fremde Informationen zu überwachen
+              oder nach Umständen zu forschen, die auf eine rechtswidrige
+              Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung
+              der Nutzung von Informationen nach den allgemeinen Gesetzen
+              bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch
+              erst ab dem Zeitpunkt der Kenntnis einer konkreten
+              Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden
+              Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+              <br></br>
+              <br></br>
+              <b>Haftung für Links</b>
+              <br></br>
+              Unser Angebot enthält Links zu externen Webseiten Dritter, auf
+              deren Inhalte wir keinen Einfluss haben. Deshalb können wir für
+              diese fremden Inhalte auch keine Gewähr übernehmen. Für die
+              Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter
+              oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten
+              wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße
+              überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der
+              Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle
+              der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer
+              Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von
+              Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+              <br></br>
+              <br></br>
+              <b>Urheberrecht</b>
+              <br></br>
+              Die durch die Seitenbetreiber erstellten Inhalte und Werke auf
+              diesen Seiten unterliegen dem deutschen Urheberrecht. Die
+              Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
+              Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der
+              schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+              Downloads und Kopien dieser Seite sind nur für den privaten, nicht
+              kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser
+              Seite nicht vom Betreiber erstellt wurden, werden die
+              Urheberrechte Dritter beachtet. Insbesondere werden Inhalte
+              Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+              Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+              entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
+              werden wir derartige Inhalte umgehend entfernen.<br></br>
+              <br></br>
+              <b>Datenschutz</b>
+              <br></br>
+              Die Nutzung unserer Webseite ist in der Regel ohne Angabe
+              personenbezogener Daten möglich. Soweit auf unseren Seiten
+              personenbezogene Daten (beispielsweise Name, Anschrift oder
+              eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich,
+              stets auf freiwilliger Basis. Diese Daten werden ohne Ihre
+              ausdrückliche Zustimmung nicht an Dritte weitergegeben. Wir weisen
+              darauf hin, dass die Datenübertragung im Internet (z.B. bei der
+              Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein
+              lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist
+              nicht möglich.<br></br>
+              Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten
+              Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich
+              angeforderter Werbung und Informationsmaterialien wird hiermit
+              ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich
+              ausdrücklich rechtliche Schritte im Falle der unverlangten
+              Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
+            </p>
+          </div>
+        </div>
+        <div id={styles.container5}>
           <div id={styles.minimues}>
-          <img
-                src="/minimues.svg"
-                alt="MiniMüs"
-                height="200"
-                width="200"
-              />
+            <img src="/minimues.svg" alt="MiniMüs" height="200" width="200" />
           </div>
           <div className={styles.row}>
-          <div className={styles.item}>
-            <a href="mailto:hallo@mut-zum-mammut.de"><h2>Kontakt</h2></a>
+            <div className={styles.item}>
+              <a href="mailto:hallo@mut-zum-mammut.de">
+                <h2>Kontakt</h2>
+              </a>
+            </div>
+            <div className={styles.item}>
+              <a href="https://instagram.com/mut_zum_mammut/">
+                <h2>Instagram</h2>
+              </a>
+            </div>
           </div>
-          <div className={styles.item}>
-            <a href="https://instagram.com/mut_zum_mammut/"><h2>Instagram</h2></a>
+          <div className={styles.row} id={styles.footer}>
+            <div className={styles.item}>
+              <p>© Minimüs 2021</p>
+            </div>
+            <div className={styles.item}>
+              <Link href="/impressum">
+                <p>Impressum</p>
+              </Link>
+            </div>
+            <div className={styles.item}></div>
           </div>
-          </div>
-          <div className={styles.row}  id={styles.footer}
-          >
-          <div className={styles.item}>
-            <p>© Minimüs 2021</p>
-          </div>
-          <div className={styles.item}>
-        <Link href="/impressum">
-        <p>Impressum</p>
-        </Link>
         </div>
-        <div className={styles.item}>
-        </div>
-          </div>
-          </div>
-        </main>
-  
-        <footer className={styles.footer}>
-        </footer>
-      </div>
-    )
-  }
-  
-export default Impressum
+      </main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
+
+export default Impressum;
